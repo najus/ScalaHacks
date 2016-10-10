ad246589c36996be1877fd7efa0ca5548180969f
Sujan Z Thapa <najussujan@gmail.com>
2016-10-10 14:03:24 -0400

    kafka initial

---
+++ src/kafka/KafkaWordCount.scala
@@ -0,0 +1,10 @@
+package kafka
+
+object KafkaWordCount {
+  def main(args: Array[String]) {
+    if (args.length < 4) {
+      System.err.println("Usage: KafkaWordCount <zkQuorum><group> <topics> <numThreads>")
+      System.exit(1)
+    }
+  }
+}
